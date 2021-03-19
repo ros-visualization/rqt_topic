d33a39dab43e4a55415d6581ab7b1a9aa7f3c00d
Scott K Logan <logans@cottsay.net>
2021-03-19 13:40:08 -0700

    Revert an unrelated line change
    Signed-off-by: Scott K Logan <logans@cottsay.net>

--- setup.py
+++ setup.py
@@ -9,8 +9,7 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
-        ('share/' + package_name + '/resource',
-            ['resource/TopicWidget.ui']),
+        ('share/' + package_name + '/resource', ['resource/TopicWidget.ui']),
         ('share/' + package_name, ['package.xml']),
         ('share/' + package_name, ['plugin.xml']),
     ],
