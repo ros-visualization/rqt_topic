57f4e0133ee191dae8c569912d1a278c471678fb
mosfet80 <10235105+mosfet80@users.noreply.github.com>
2025-07-05 09:53:30 +0200

    fix setuptools deprecations
    FIX:
    
    #UserWarning: Unknown distribution option: 'tests_require'
    
    and
    
    #SetuptoolsDeprecationWarning: License classifiers are deprecated. !!
    
    ********************************************************************************
    Please consider removing the following classifiers in favor of a SPDX license expression:
    
    License :: OSI Approved :: BSD License
    
    See https://packaging.python.org/en/latest/guides/writing-pyproject-toml/#license for details.
    ********************************************************************************

--- setup.py
+++ setup.py
@@ -21,7 +21,6 @@ setup(
     keywords=['ROS'],
     classifiers=[
         'Intended Audience :: Developers',
-        'License :: OSI Approved :: BSD License',
         'Programming Language :: Python',
         'Topic :: Software Development',
     ],
@@ -30,7 +29,11 @@ setup(
         'including publishers, subscribers, publishing rate, and ROS Messages.'
     ),
     license='BSD',
-    tests_require=['pytest'],
+    extras_require={
+        'test': [
+            'pytest'
+        ]
+    },
     entry_points={
         'console_scripts': [
             'rqt_topic = ' + package_name + '.main:main',
