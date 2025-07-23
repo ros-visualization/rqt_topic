46cc99451890119fb8c448fc9c9f34569acfa7b1
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2025-07-23 14:08:05 +0200

    Added copyright
    Signed-off-by: Alejandro Hernandez Cordero <ahcorde@gmail.com>

--- test/test_copyright.py
+++ test/test_copyright.py
@@ -1,4 +1,4 @@
-# Copyright 2025 Open Source Robotics Foundation, Inc.
+# Copyright (c) 2025, Open Source Robotics Foundation, Inc.
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -11,7 +11,7 @@
 #     copyright notice, this list of conditions and the following
 #     disclaimer in the documentation and/or other materials provided
 #     with the distribution.
-#   * Neither the name of the Willow Garage, Inc. nor the names of its
+#   * Neither the name of the copyright holder nor the names of its
 #     contributors may be used to endorse or promote products derived
 #     from this software without specific prior written permission.
 #
@@ -28,64 +28,11 @@
 # ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE.
 
+from ament_copyright.main import main
 import pytest
 
-from rqt_topic.models.topic import (
-    Bandwidth,
-    Frequency,
-    generate_test_topics,
-)
 
-NUMBER_OF_TOPICS = 15
-
-
-@pytest.fixture
-def topics():
-    return generate_test_topics(NUMBER_OF_TOPICS)
-
-
-def test_bandwidth():
-    bw = Bandwidth()
-    bw.fill(
-        bytes_per_sec=25.0,
-        samples=10,
-        mean=30.0,
-        min_size=1.0,
-        max_size=50.0,
-    )
-
-    assert bw.bytes_per_sec == 25.0
-    assert bw.samples == 10
-    assert bw.mean == 30.0
-    assert bw.min_size == 1.0
-    assert bw.max_size == 50.0
-
-    bw.clear()
-
-    assert bw.bytes_per_sec == 0.0
-
-
-def test_frequency():
-    fq = Frequency()
-    fq.fill(
-        rate=0.00000001,
-        min_delta=8.6,
-        max_delta=7.5,
-        std_dev=3.0,
-        samples=9,
-    )
-
-    assert fq.rate == 10.0
-    assert fq.min_delta == 8.6
-    assert fq.max_delta == 7.5
-    assert fq.std_dev == 3.0
-    assert fq.samples == 9
-
-
-def test_topic_model(topics):
-    assert len(topics) == NUMBER_OF_TOPICS
-    for i, topic in enumerate(topics):
-        assert topic.name == f'/{i}/test_topic'
-        assert topic.message_type == 'test_msgs/BasicTypes'
-        assert topic.bandwidth.bytes_per_sec == float(i)
-        assert topic.frequency.rate == float(i)
+@pytest.mark.copyright
+@pytest.mark.linter
+def test_copyright():
+    assert main() == 0, 'Found errors'
