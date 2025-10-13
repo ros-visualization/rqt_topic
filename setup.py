ffc5380aa6ceccf51d3ee78df5f860b4e4b233d9
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2025-10-13 10:07:16 +0200

    2.0.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 package_name = 'rqt_topic'
 setup(
     name=package_name,
-    version='2.0.0',
+    version='2.0.1',
     package_dir={'': '.'},
     packages=[
         package_name,
