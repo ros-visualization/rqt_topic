8da64fd086215af6fb0ba0d69e3a633af0685426
Alejandro Hernandez Cordero <ahcorde@gmail.com>
2026-05-06 17:21:58 +0200

    3.0.0

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 package_name = 'rqt_topic'
 setup(
     name=package_name,
-    version='2.1.0',
+    version='3.0.0',
     package_dir={'': '.'},
     packages=[
         package_name,
